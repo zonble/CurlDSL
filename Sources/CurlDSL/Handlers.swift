6ed6f8701ed2f4c2e07854d20db3abffad2abc96
zonble <zonble@gmail.com>
2019-08-03 21:16:00 +0800

    Adds additional handlers.

--- Sources/CurlDSL/Handlers.swift
+++ Sources/CurlDSL/Handlers.swift
@@ -7,13 +7,71 @@ public enum HandlerError: Error {
 	case invalidFormat
 }
 
-public class Handler<T:Any> {
+public class Handler<T> {
 	var callback: Callback<T>
 	public init(_ callback:@escaping Callback<T>) {
 		self.callback = callback
 	}
 	public func handle(_: Data?, _: URLResponse?, _: Error?) {
 		fatalError("Not implemented")
+	}
+}
+
+public class CodableHandler<T:Codable>: Handler<T> {
+	public var keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys
+	public var dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
+	public var dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData
+	public var nonConformingFloatDecodingStrategy: JSONDecoder.NonConformingFloatDecodingStrategy = .throw
+
+	public override func handle(_ data: Data?, _ response: URLResponse?, _ apiError: Error?) {
+		if let apiError = apiError {
+			DispatchQueue.main.async {
+				self.callback(.failure(apiError))
+			}
+			return
+		}
+		guard let data = data else {
+			DispatchQueue.main.async {
+				self.callback(.failure(HandlerError.noData))
+			}
+			return
+		}
+		let decoder = JSONDecoder()
+		decoder.keyDecodingStrategy = keyDecodingStrategy
+		decoder.dateDecodingStrategy = dateDecodingStrategy
+		decoder.dataDecodingStrategy = dataDecodingStrategy
+		decoder.nonConformingFloatDecodingStrategy = nonConformingFloatDecodingStrategy
+
+		do {
+			let object = try decoder.decode(T.self, from: data)
+			DispatchQueue.main.async {
+				self.callback(.success(object))
+			}
+		} catch {
+			DispatchQueue.main.async {
+				self.callback(.failure(error))
+			}
+		}
+	}
+}
+
+public class DataHandler: Handler <Data> {
+	public override func handle(_ data: Data?, _ response: URLResponse?, _ apiError: Error?) {
+		if let apiError = apiError {
+			DispatchQueue.main.async {
+				self.callback(.failure(apiError))
+			}
+			return
+		}
+		guard let data = data else {
+			DispatchQueue.main.async {
+				self.callback(.failure(HandlerError.noData))
+			}
+			return
+		}
+		DispatchQueue.main.async {
+			self.callback(.success(data))
+		}
 	}
 }
 
@@ -47,6 +105,4 @@ public class JsonDictionaryHandler: Handler <[AnyHashable:Any]> {
 			}
 		}
 	}
-
 }
-
